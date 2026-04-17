6325c01a0ecb81b52d0d54ac97599e1cc2e38991
wangcong0355 <3540475431@qq.com>
2026-04-17 20:23:47 +0800

    docs: 为NoteWidgetProvider_2x.java添加中文注释

--- app/src/main/java/net/micode/notes/widget/NoteWidgetProvider_2x.java
+++ app/src/main/java/net/micode/notes/widget/NoteWidgetProvider_2x.java
@@ -23,23 +23,40 @@ import net.micode.notes.R;
 import net.micode.notes.data.Notes;
 import net.micode.notes.tool.ResourceParser;
 
-
+// NoteWidgetProvider_2x - Widget provider for 2x size widget
+/* 2x4尺寸桌面小部件提供者 - 继承自NoteWidgetProvider，实现2x4尺寸的小部件 */
 public class NoteWidgetProvider_2x extends NoteWidgetProvider {
+    
+    // Called when widget is updated
+    /* 当小部件更新时调用 */
+    /* @param context 上下文对象 */
+    /* @param appWidgetManager 小部件管理器 */
+    /* @param appWidgetIds 需要更新的小部件ID数组 */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.update(context, appWidgetManager, appWidgetIds);
     }
 
+    // Get the layout resource id for 2x widget
+    /* 获取2x尺寸小部件的布局资源ID */
+    /* @return 布局资源ID */
     @Override
     protected int getLayoutId() {
         return R.layout.widget_2x;
     }
 
+    // Get background resource by color id for 2x widget
+    /* 根据颜色ID获取2x小部件的背景图片资源 */
+    /* @param bgId 背景颜色ID */
+    /* @return 背景图片资源ID */
     @Override
     protected int getBgResourceId(int bgId) {
         return ResourceParser.WidgetBgResources.getWidget2xBgResource(bgId);
     }
 
+    // Get widget type constant
+    /* 获取小部件类型常量 */
+    /* @return 小部件类型（2x尺寸） */
     @Override
     protected int getWidgetType() {
         return Notes.TYPE_WIDGET_2X;
